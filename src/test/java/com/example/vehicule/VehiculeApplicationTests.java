a284839068aa16cc0b2f174abea502a481a78311
christian-Tooj-R <118376824+christian-Tooj-R@users.noreply.github.com>
2023-11-28 10:29:36 +0300

    all changes

--- src/test/java/com/example/vehicule/VehiculeApplicationTests.java
+++ src/test/java/com/example/vehicule/VehiculeApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.vehicule;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NbaApplicationTests {
+class VehiculeApplicationTests {
 
 	@Test
 	void contextLoads() {
